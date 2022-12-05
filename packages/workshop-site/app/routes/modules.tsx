41ea34457e27ecfe726251e4da1419267422573e
tjuranek <thomas@juranek.com>
2022-12-04 18:54:31 -0700

    sidebar update

--- packages/workshop-site/app/routes/modules.tsx
+++ packages/workshop-site/app/routes/modules.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from '@remix-run/react';
-import { CheckOutline, Home } from '~/icons';
+import { CheckOutline, Home, CheckFill } from '~/icons';
 import moduleStyles from '~/styles/routes/modules.css';
 import contentStyles from '~/styles/routes/module-content.css';
 
@@ -9,6 +9,82 @@ export function links() {
     { rel: 'stylesheet', href: contentStyles }
   ];
 }
+
+function SidebarHeading({ text }: { text: string }) {
+  return <h3 className="sidebar-heading">{text}</h3>;
+}
+
+function SidebarItem({
+  text,
+  link,
+  checked
+}: {
+  text: string;
+  link: string;
+  checked: boolean;
+}) {
+  return (
+    <li>
+      <Link className="sidebar-link" to={link} prefetch="intent">
+        {checked ? <CheckFill /> : <CheckOutline />}
+        {text}
+      </Link>
+    </li>
+  );
+}
+
+const sidebarContent = [
+  {
+    heading: 'Introduction',
+    items: [
+      {
+        text: 'Prerequisites',
+        link: '/modules/introduction/prerequisites'
+      },
+      {
+        text: 'Scenario',
+        link: '/modules/introduction/scenario'
+      },
+      {
+        text: 'Define a Tech Stack',
+        link: '/modules/introduction/define-a-tech-stack'
+      }
+    ]
+  },
+  {
+    heading: 'Remix Basics',
+    items: [
+      {
+        text: 'What is a PESPA?',
+        link: '/modules/remix-basics/what-is-a-pespa'
+      },
+      {
+        text: 'Why Remix?',
+        link: '/modules/remix-basics/why-remix'
+      },
+      {
+        text: 'Creating a Project',
+        link: '/modules/remix-basics/creating-a-project'
+      },
+      {
+        text: 'Routing',
+        link: '/modules/remix-basics/routing'
+      },
+      {
+        text: 'Data Fetching',
+        link: '/modules/remix-basics/data-fetching'
+      },
+      {
+        text: 'Data Mutation',
+        link: '/modules/remix-basics/data-mutation'
+      },
+      {
+        text: 'In Review',
+        link: '/modules/remix-basics/in-review'
+      }
+    ]
+  }
+];
 
 export default function Modules() {
   return (
@@ -24,104 +100,23 @@ export default function Modules() {
       </div>
 
       <div className="sidebar">
-        <h3>Introduction</h3>
+        {sidebarContent.map((section, sectionIndex) => (
+          <div key={sectionIndex} className="sidebar-section">
+            <SidebarHeading text={section.heading} />
+            <hr />
 
-        <hr />
-
-        <ul className="sidebar-list">
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Prerequisites
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Scenario
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Defining a Tech Stack
-            </Link>
-          </li>
-        </ul>
-
-        <h3>Remix Basics</h3>
-
-        <hr />
-
-        <ul className="sidebar-list">
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              What is a PEPSA?
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Why Remix?
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Routing
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Data Fetching
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="sidebar-link"
-              to="/modules/introduction/scenario"
-              prefetch="intent"
-            >
-              <CheckOutline />
-              Data Mutation
-            </Link>
-          </li>
-        </ul>
+            <ul className="sidebar-list">
+              {section.items.map((item, itemIndex) => (
+                <SidebarItem
+                  key={`${sectionIndex} - ${itemIndex}`}
+                  text={item.text}
+                  link={item.link}
+                  checked={true}
+                />
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
 
       <div className="content">
